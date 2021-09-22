c8b573077ebe9688cfaff3e40a36f250067cefb2
dmccullor <dmccullor0@gmail.com>
2021-09-22 00:49:55 -0500

    working sine wave plot

--- logic.js
+++ logic.js
@@ -1,12 +1,35 @@
+var sampleRate = 1000
+var frequency = 440;
+var amplitude = 0.01;
+var theta = 0;
+
+var timeArr = [];
+
+for (var i = 0; i < sampleRate; i++) {
+  timeArr.push(i + 0.01);
+}
+  
+
+//function makeSine(inputTime) {
+  var sineArr = []
+  for (var j = 0; j < sampleRate; j++) {
+    sineArr.push(amplitude * Math.sin(((2 * Math.PI) * (1/frequency)) * j + theta));
+  }
+  //return sineArr;
+//}
+
+console.log(sineArr);
+console.log(timeArr);
+
 // Create our first trace
 var trace1 = {
-    x: [1, 2, 3, 4, 5],
-    y: randomNumbersBetween0and9(5),
+    x: timeArr,
+    y: sineArr,
     type: "scatter"
   };
   
   // The data array consists of both traces
-  var data = [trace1, trace2];
+  var data = [trace1];
   
   // Note that we omitted the layout object this time
   // This will use default parameters for the layout
