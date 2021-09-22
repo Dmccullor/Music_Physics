b8359bd3ab32565862b77db50818d2d34ab44a54
dmccullor <dmccullor0@gmail.com>
2021-09-22 18:47:59 -0500

    Adding notes object and traces

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
+var notes = [{"C": 261.626},{"C#/Db": 277.183}, {"D": 293.665}, {"D#/Eb": 311.127},
+  {"E": 329.628}, {"F": 349.228}, {"F#/Gb": 369.994}, {"A": 440}, {"A#/Bb": 466.164},
+{"B": 493.883}];
+
 var sampleRate = 2000;
-var frequency = 440;
-//var amplitude = 0.01;
 
 var timeArr = [];
 
@@ -15,14 +17,15 @@ console.log(timeArr);
 function makeSine(freq, amp) {
   let sineArr = []
   for (var j = 0; j <= sampleRate; j++) {
-    sineArr.push(amp * Math.sin(((2 * Math.PI) * freq) * timeArr[j]));
+    sineArr.push(amp * Math.sin(((2 * Math.PI) * (freq/100)) * timeArr[j]));
   }
   return sineArr;
 }
 
-var sineWave = makeSine(2, 30);
-var octaveWave = makeSine(4, 15);
-var fifthWave = makeSine(3, 23);
+var sineWave = makeSine(256, 30);
+var octaveWave = makeSine(512, 15);
+var fifthWave = makeSine(384, 23);
+var thirdWave = makeSine(320, 27)
 
 console.log(sineWave)
 
@@ -30,13 +33,15 @@ console.log(sineWave)
 var trace1 = {
   x: timeArr,
   y: sineWave,
-  type: "scatter"
+  type: "scatter",
+  name: "Root Note"
 };
 
 var trace2 = {
   x: timeArr,
   y: octaveWave,
   type: "scatter",
+  name: "Octave",
   opacity: 0.25
 };
 
@@ -44,11 +49,26 @@ var trace3 = {
   x: timeArr,
   y: fifthWave,
   type: "scatter",
+  name: "Fifth",
   opacity: 0.5
+}
+
+var trace4 = {
+  x: timeArr,
+  y: thirdWave,
+  type: "scatter",
+  name: "Third",
+  opacity: 0.75
 }
   
 
-  var data = [trace1, trace2, trace3];
+var data = [trace1, trace4, trace3, trace2];
+
+var layout = {
+  title: "<b>Musical Note Sinewaves</b>",
+  xaxis: {title: {text: "Time (ms)"}},
+  yaxis: {title: {text: "Decibels"}}
+};
   
 
-  Plotly.newPlot("plot", data);
+Plotly.newPlot("plot", data, layout);
